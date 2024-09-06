faeb70e0e616d25b5228b0257679740000c1917c
Shawn <veckshawn@gmail.com>
2024-09-06 14:46:25 +0800

    chore: CI

--- Ui/AppVersion.cs
+++ Ui/AppVersion.cs
@@ -8,7 +8,7 @@ namespace _1RM
         public const uint Minor = 9;
         public const uint Patch = 0;
         public const uint Build = 0;
-        public const string PreRelease = "alpha.01"; // e.g. "alpha" "beta.2"
+        public const string PreRelease = ""; // e.g. "alpha" "beta.2"
 
 
         public static readonly VersionHelper.Version VersionData = new VersionHelper.Version(Major, Minor, Patch, Build, PreRelease);
